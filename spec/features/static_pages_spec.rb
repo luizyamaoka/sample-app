6c72bc6efbbeacd54dce365f4b61e407b769bbfa
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2013-12-12 21:07:30 -0200

    Finishing static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -7,6 +7,12 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			page.should have_content('Sample App')
 		end
+
+		it "should have the right h1" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', 
+				:text => "Sample App")
+		end
 	end
 
 	describe "Help page" do
@@ -14,12 +20,24 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			page.should have_content('Help')
 		end
+
+		it "should have the right h1" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', 
+				:text => "Help")
+		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_content('About Us')
+		end
+
+		it "should have the right h1" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', 
+				:text => "About Us")
 		end
 	end
 
